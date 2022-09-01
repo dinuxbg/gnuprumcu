74b47c2786e739aff195fa29edc4c65b01bfd9b3
Dimitar Dimitrov <dimitar@dinux.eu>
2022-09-01 22:24:57 +0300

    tda4vm: Fix and update the header file
     - Fix offsets, gaps and bit lengths in the definitions.
     - Remove the _OFFS macros. They don't seem to be useful.
     - Update field names to latest PRU document for TDA4VM.
    
    Signed-off-by: Dimitar Dimitrov <dimitar@dinux.eu>

--- include/pru/tda4vm.h
+++ include/pru/tda4vm.h
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: BSD-2-Clause-FreeBSD
  *
  * pru/tda4vm.h -- PRU I/O registers definitions for PRU inside TDA4 SoCs
- * Source: SPRUIL1B – MAY 2019 – REVISED DECEMBER 2020
+ * Source: SPRUJ61 – AUGUST 2022
  *
  * Copyright (c) 2022 Dimitar Dimitrov <dimitar@dinux.eu>
  * All rights reserved.
@@ -12,7 +12,7 @@
 
 
 /* Local memory map
- * TRM section "6.6.4.4.1.2 PRU_ICSSG Local Data Memory Map".
+ * Section "5.4.1.2 PRU_ICSSG Local Data Memory Map"
  */
 #define __LOCAL_DATA_RAM_BASE		0x00000000
 #define __PEER_DATA_RAM_BASE		0x00002000
@@ -101,16 +101,6 @@
 /* 24-31 are user-defined base addresses. */
 
 /* ====================== PRU CTRL module ====================== */
-#define PRU_CTRL_CONTROL_OFFS		0x00
-#define PRU_CTRL_STATUS_OFFS		0x04
-#define PRU_CTRL_WAKEUP_EN_OFFS		0x08
-#define PRU_CTRL_CYCLE_OFFS		0x0c
-#define PRU_CTRL_STALL_OFFS		0x10
-#define PRU_CTRL_CTBIR0_OFFS		0x20
-#define PRU_CTRL_CTBIR1_OFFS		0x24
-#define PRU_CTRL_CPPR0_OFFS		0x28
-#define PRU_CTRL_CPPR1_OFFS		0x2c
-
 struct __PRU_CONTROL_S {
 	const volatile unsigned int SOFT_RST_N		: 1;
 	volatile unsigned int ENABLE			: 1;
@@ -126,22 +116,22 @@ struct __PRU_CONTROL_S {
 };
 
 struct __PRU_STATUS_S {
-	const volatile unsigned int PCTR		: 16;
+	const volatile unsigned int PCOUNTER		: 16;
 	const volatile unsigned int __reserved_1	: 16;
 };
 
 struct __PRU_CTBIR0_S {
 	volatile unsigned int C24_BLK_INDEX		: 8;
-	volatile unsigned int __reserved_1		: 8;
+	const volatile unsigned int __reserved_1	: 8;
 	volatile unsigned int C25_BLK_INDEX		: 8;
-	volatile unsigned int __reserved_2		: 8;
+	const volatile unsigned int __reserved_2	: 8;
 };
 
 struct __PRU_CTBIR1_S {
 	volatile unsigned int C26_BLK_INDEX		: 8;
-	volatile unsigned int __reserved_1		: 8;
+	const volatile unsigned int __reserved_1	: 8;
 	volatile unsigned int C27_BLK_INDEX		: 8;
-	volatile unsigned int __reserved_2		: 8;
+	const volatile unsigned int __reserved_2	: 8;
 };
 
 struct __PRU_CTPPR0_S {
@@ -211,83 +201,6 @@ struct __PRU_CTRL {
 
 /* ====================== ICSS INTC module ====================== */
 
-#define ICSS_INTC_REVID_OFFS		0x00
-#define ICSS_INTC_CR_OFFS		0x04
-#define ICSS_INTC_GER_OFFS		0x10
-#define ICSS_INTC_GNLR_OFFS		0x1C
-#define ICSS_INTC_SISR_OFFS		0x20
-#define ICSS_INTC_SICR_OFFS		0x24
-#define ICSS_INTC_EISR_OFFS		0x28
-#define ICSS_INTC_EICR_OFFS		0x2c
-#define ICSS_INTC_HIEISR_OFFS		0x34
-#define ICSS_INTC_HIDISR_OFFS		0x38
-#define ICSS_INTC_GPIR_OFFS		0x80
-#define ICSS_INTC_SRSR0_OFFS		0x200
-#define ICSS_INTC_SRSR1_OFFS		0x204
-#define ICSS_INTC_SRSR2_OFFS		0x208
-#define ICSS_INTC_SRSR3_OFFS		0x20C
-#define ICSS_INTC_SRSR4_OFFS		0x210
-#define ICSS_INTC_SECR0_OFFS		0x280
-#define ICSS_INTC_SECR1_OFFS		0x284
-#define ICSS_INTC_SECR2_OFFS		0x288
-#define ICSS_INTC_SECR3_OFFS		0x28C
-#define ICSS_INTC_SECR4_OFFS		0x290
-#define ICSS_INTC_ESR0_OFFS		0x300
-#define ICSS_INTC_ERS1_OFFS		0x304
-#define ICSS_INTC_ERS2_OFFS		0x308
-#define ICSS_INTC_ERS3_OFFS		0x30C
-#define ICSS_INTC_ERS4_OFFS		0x310
-#define ICSS_INTC_ECR0_OFFS		0x380
-#define ICSS_INTC_ECR1_OFFS		0x384
-#define ICSS_INTC_ECR2_OFFS		0x388
-#define ICSS_INTC_ECR3_OFFS		0x38C
-#define ICSS_INTC_ECR4_OFFS		0x390
-#define ICSS_INTC_CMR0_OFFS		0x400
-#define ICSS_INTC_CMR1_OFFS		0x404
-#define ICSS_INTC_CMR2_OFFS		0x408
-#define ICSS_INTC_CMR3_OFFS		0x40c
-#define ICSS_INTC_CMR4_OFFS		0x410
-#define ICSS_INTC_CMR5_OFFS		0x414
-#define ICSS_INTC_CMR6_OFFS		0x418
-#define ICSS_INTC_CMR7_OFFS		0x41c
-#define ICSS_INTC_CMR8_OFFS		0x420
-#define ICSS_INTC_CMR9_OFFS		0x424
-#define ICSS_INTC_CMR10_OFFS		0x428
-#define ICSS_INTC_CMR11_OFFS		0x42c
-#define ICSS_INTC_CMR12_OFFS		0x430
-#define ICSS_INTC_CMR13_OFFS		0x434
-#define ICSS_INTC_CMR14_OFFS		0x438
-#define ICSS_INTC_CMR15_OFFS		0x43c
-#define ICSS_INTC_HMR0_OFFS		0x800
-#define ICSS_INTC_HMR1_OFFS		0x804
-#define ICSS_INTC_HMR2_OFFS		0x808
-#define ICSS_INTC_HIPIR0_OFFS		0x900
-#define ICSS_INTC_HIPIR1_OFFS		0x904
-#define ICSS_INTC_HIPIR2_OFFS		0x908
-#define ICSS_INTC_HIPIR3_OFFS		0x90c
-#define ICSS_INTC_HIPIR4_OFFS		0x910
-#define ICSS_INTC_HIPIR5_OFFS		0x914
-#define ICSS_INTC_HIPIR6_OFFS		0x918
-#define ICSS_INTC_HIPIR7_OFFS		0x91c
-#define ICSS_INTC_HIPIR8_OFFS		0x920
-#define ICSS_INTC_HIPIR9_OFFS		0x924
-#define ICSS_INTC_SIPR0_OFFS		0xd00
-#define ICSS_INTC_SIPR1_OFFS		0xd04
-#define ICSS_INTC_SITR0_OFFS		0xd80
-#define ICSS_INTC_SITR1_OFFS		0xd84
-#define ICSS_INTC_HINLR0_OFFS		0x1100
-#define ICSS_INTC_HINLR1_OFFS		0x1104
-#define ICSS_INTC_HINLR2_OFFS		0x1108
-#define ICSS_INTC_HINLR3_OFFS		0x110c
-#define ICSS_INTC_HINLR4_OFFS		0x1110
-#define ICSS_INTC_HINLR5_OFFS		0x1114
-#define ICSS_INTC_HINLR6_OFFS		0x1118
-#define ICSS_INTC_HINLR7_OFFS		0x111c
-#define ICSS_INTC_HINLR8_OFFS		0x1120
-#define ICSS_INTC_HINLR9_OFFS		0x1124
-#define ICSS_INTC_HIER_OFFS		0x1500
-
-
 struct __ICSS_INTC_REVID_S {
 	const volatile unsigned int REV_MINOR		: 6;
 	const volatile unsigned int REV_CUSTOM		: 2;
@@ -317,7 +230,7 @@ struct __ICSS_INTC_GER_S {
 struct __ICSS_INTC_GNLR_S {
 	volatile unsigned int GLB_NEST_LEVEL		: 9;
 	volatile unsigned int __reserved_1		: 22;
-	volatile unsigned int AUTO_OVERRIDE		: 1;
+	volatile unsigned int GLB_NEST_AUTO_OVR		: 1;
 };
 
 
@@ -347,6 +260,11 @@ struct __ICSS_INTC_EICR_S {
 
 struct __ICSS_INTC_HIEISR_S {
 	volatile unsigned int HINT_ENABLE_SET_INDEX	: 10;
+	volatile unsigned int __reserved_1		: 22;
+};
+
+struct __ICSS_INTC_HIEICR_S {
+	volatile unsigned int HINT_ENABLE_CLR_INDEX	: 10;
 	volatile unsigned int __reserved_1		: 22;
 };
 
@@ -478,7 +396,7 @@ struct __ICSS_INTC {
 	};
 	union { /* 0x38 */
 		volatile unsigned int HINT_ENABLE_CLR_INDEX_REG;
-		struct __ICSS_INTC_HIEISR_S HINT_ENABLE_CLR_INDEX_REG_bit;
+		struct __ICSS_INTC_HIEICR_S HINT_ENABLE_CLR_INDEX_REG_bit;
 	};
 	unsigned int __gap4[17];
 	union { /* 0x80 */
@@ -556,7 +474,7 @@ struct __ICSS_INTC {
 	__ICSS_INTC_HMR_decl(2, 8,9,10,11);
 	__ICSS_INTC_HMR_decl(3, 12,13,14,15);
 	__ICSS_INTC_HMR_decl(4, 16,17,18,19);
-	unsigned int __gap11[256 - 5];
+	unsigned int __gap11[59];
 	__ICSS_INTC_PRI_HINT_decl(0);		/* 0x900 */
 	__ICSS_INTC_PRI_HINT_decl(1);
 	__ICSS_INTC_PRI_HINT_decl(2);
@@ -583,7 +501,13 @@ struct __ICSS_INTC {
 	volatile unsigned int POLARITY_REG2;
 	volatile unsigned int POLARITY_REG3;
 	volatile unsigned int POLARITY_REG4;
-	unsigned int __gap13[0x1100/4 - 0xD00/4 - 5];
+	unsigned int __gap13[27];
+	volatile unsigned int TYPE_REG0;	/* 0xD80 */
+	volatile unsigned int TYPE_REG1;
+	volatile unsigned int TYPE_REG2;
+	volatile unsigned int TYPE_REG3;
+	volatile unsigned int TYPE_REG4;
+	unsigned int __gap14[219];
 	__ICSS_INTC_NEST_LEVEL_decl(0);	/* 0x1100 */
 	__ICSS_INTC_NEST_LEVEL_decl(1);
 	__ICSS_INTC_NEST_LEVEL_decl(2);
@@ -604,7 +528,7 @@ struct __ICSS_INTC {
 	__ICSS_INTC_NEST_LEVEL_decl(17);
 	__ICSS_INTC_NEST_LEVEL_decl(18);
 	__ICSS_INTC_NEST_LEVEL_decl(19);
-	unsigned int __gap14[0x1500/4 - 0x1100/4 - 20];
+	unsigned int __gap15[0x1500/4 - 0x1100/4 - 20];
 	union {	/* 0x1500 */
 		volatile unsigned int ENABLE_HINT_REG0;
 		struct __ICSS_INTC_HIER_S ENABLE_HINT_REG0_bit;
@@ -620,24 +544,11 @@ struct __ICSS_INTC {
 
 /* ====================== ICSSG CFG module ====================== */
 
-#define ICSSG_CFG_PID_OFFS		0x00
-#define ICSSG_CFG_HWDIS_OFFS		0x04
-#define ICSSG_CFG_GPCFG0_OFFS		0x08
-#define ICSSG_CFG_GPCFG1_OFFS		0x0c
-#define ICSSG_CFG_CGR_OFFS		0x10
-#define ICSSG_CFG_GPECFG0_OFFS		0x14
-#define ICSSG_CFG_GPECFG1_OFFS		0x18
-#define ICSSG_CFG_MII_RT_OFFS		0x2c
-#define ICSSG_CFG_IEPCLK_OFFS		0x30
-#define ICSSG_CFG_SPP_OFFS		0x34
-#define ICSSG_CFG_CORE_SYNC_OFFS	0x3c
-#define ICSSG_CFG_SA_MX_OFFS		0x40
-
 struct __ICSSG_CFG_PID_REG_S {
 	volatile unsigned int REVMIN			: 6;
 	volatile unsigned int CUSTOM			: 2;
 	volatile unsigned int REVMAJ			: 3;
-	volatile unsigned int REVRTL			: 8;
+	volatile unsigned int REVRTL			: 5;
 	volatile unsigned int MODID			: 16;
 };
 
@@ -655,9 +566,9 @@ struct __ICSSG_CFG_GPCFGx_S {
 	volatile unsigned int GPO_MODE			: 1;
 	volatile unsigned int GPO_DIV0			: 5;
 	volatile unsigned int GPO_DIV1			: 5;
-	volatile unsigned int GPO_SH_SEL		: 1;
-	volatile unsigned int PR1_PRU0_GP_MUX_SEL	: 4;
-	volatile unsigned int __reserved_1		: 6;
+	volatile unsigned int GPO_SH1_SEL		: 1;
+	volatile unsigned int PR1_GP_MUX_SEL		: 4;
+	volatile unsigned int __reserved_1		: 2;
 };
 
 struct __ICSSG_CFG_GPCFG0_S {
@@ -669,9 +580,9 @@ struct __ICSSG_CFG_GPCFG0_S {
 	volatile unsigned int PRU0_GPO_MODE		: 1;
 	volatile unsigned int PRU0_GPO_DIV0		: 5;
 	volatile unsigned int PRU0_GPO_DIV1		: 5;
-	volatile unsigned int PRU0_GPO_SH_SEL		: 1;
-	volatile unsigned int PRU0_PR1_PRU0_GP_MUX_SEL	: 4;
-	volatile unsigned int PRU0___reserved_1		: 6;
+	volatile unsigned int PRU0_GPO_SH1_SEL		: 1;
+	volatile unsigned int PR1_PRU0_GP_MUX_SEL	: 4;
+	volatile unsigned int PRU0___reserved_1		: 2;
 };
 
 struct __ICSSG_CFG_GPCFG1_S {
@@ -683,9 +594,9 @@ struct __ICSSG_CFG_GPCFG1_S {
 	volatile unsigned int PRU1_GPO_MODE		: 1;
 	volatile unsigned int PRU1_GPO_DIV0		: 5;
 	volatile unsigned int PRU1_GPO_DIV1		: 5;
-	volatile unsigned int PRU1_GPO_SH_SEL		: 1;
-	volatile unsigned int PRU1_PR1_PRU0_GP_MUX_SEL	: 4;
-	volatile unsigned int PRU1___reserved_1		: 6;
+	volatile unsigned int PRU1_GPO_SH1_SEL		: 1;
+	volatile unsigned int PR1_PRU1_GP_MUX_SEL	: 4;
+	volatile unsigned int PRU1___reserved_1		: 2;
 };
 
 struct __ICSSG_CFG_CGR_S {
